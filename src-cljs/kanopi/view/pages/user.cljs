b0643ea1d6d8ef5c6c81d2e25654cc279469a63f
brianru <brianrubinton@gmail.com>
2015-09-23 19:51:45 -0400

    Enter key submits login/register form

--- src-cljs/kanopi/view/pages/user.cljs
+++ src-cljs/kanopi/view/pages/user.cljs
@@ -9,22 +9,33 @@
              :refer-macros (log trace debug info warn error fatal report)]
             [ajax.core :as http]))
 
-(defn- username-field [owner username-key]
+(defn- handle-key-down [owner submit-fn submittable evt]
+  (case (.-key evt)
+    "Enter"
+    (when submittable
+      (submit-fn))
+    
+    ;;default
+    nil))
+
+(defn- username-field [owner username-key submit-fn submittable]
   [:div.form-group
    [:input.form-control
     {:type        "text"
      :placeholder "username"
      :value       (om/get-state owner username-key)
      :on-change   #(om/set-state! owner username-key (.. % -target -value))
+     :on-key-down (partial handle-key-down owner submit-fn submittable)
      }]])
 
-(defn- password-field [owner password-key]
+(defn- password-field [owner password-key submit-fn submittable]
   [:div.form-group
    [:input.form-control
     {:type        "password"
      :placeholder "password"
      :value       (om/get-state owner password-key)
      :on-change   #(om/set-state! owner password-key (.. % -target -value))
+     :on-key-down (partial handle-key-down owner submit-fn submittable)
      }]])
 
 (defn- login! [owner creds]
@@ -70,7 +81,12 @@
 
     om/IRenderState
     (render-state [_ {:keys [username password] :as state}]
-      (let [mode (get-in props [:page :handler])]
+      (let [mode (get-in props [:page :handler])
+            submit-fn (case mode
+                        :login    #(login! owner (select-keys state [:username :password]))
+                        :register #(register! owner (select-keys state [:username :password]))
+                        :logout   #(logout! owner))
+            submittable (not (some nil? [username password]))]
         (html
          [:div.container-fluid
           [:div.row
@@ -87,25 +103,25 @@
 
               (when (#{:register :login} mode)
                 [:div
-                 (username-field owner :username)
-                 (password-field owner :password)])
+                 (username-field owner :username submit-fn submittable)
+                 (password-field owner :password submit-fn submittable)])
 
               (case mode
                 :login
                 [:button.btn.btn-primary.btn-block
-                 {:on-click #(login! owner (select-keys state [:username :password]))
-                  :disabled (some nil? [username password])}
+                 {:on-click submit-fn
+                  :disabled (not submittable)}
                  "Login"]
 
                 :register
                 [:button.btn.btn-warning.btn-block
-                 {:on-click #(register! owner (select-keys state [:username :password]))
-                  :disabled (some nil? [username password])}
+                 {:on-click submit-fn
+                  :disabled (not submittable)}
                  "Register"]
 
                 :logout
                 [:button.btn.btn-danger.btn-block
-                 {:on-click #(logout! owner)}
+                 {:on-click submit-fn}
                  "Logout"]
                 )
 
