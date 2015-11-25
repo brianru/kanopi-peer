e49ca4249fafbc5bdf26e72f2b226b54ebd30e16
brianru <brianrubinton@gmail.com>
2015-11-25 10:30:39 -0500

    port util.core to cljc

---
+++ src-cljc/kanopi/util/core.cljc
@@ -0,0 +1,167 @@
+(ns kanopi.util.core
+  #?(:cljs (:refer-clojure :exclude (random-uuid)))
+  #?(:cljs (:require [cljs-uuid-utils.core :as uuid]
+                     cljs.reader
+                     clojure.string)
+     :clj  (:require [cognitect.transit :as transit]
+                     clojure.string))
+  #?(:clj  (:import java.util.UUID
+                    [java.io ByteArrayInputStream ByteArrayOutputStream])))
+
+(defn random-uuid []
+  #?(:cljs (str (uuid/make-random-uuid))
+     :clj  (str (java.util.UUID/randomUUID))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Sequential ID Creation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def last-id (atom -111111))
+
+(defn next-id []
+  (swap! last-id dec))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Fuzzy text matching
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn- next-row
+  [previous current other-seq]
+  (reduce
+    (fn [row [diagonal above other]]
+      (let [update-val (if (= other current)
+                          diagonal
+                          (inc (min diagonal above (first row))))]
+        (conj row update-val)))
+    [(inc (first previous))]
+    (map vector previous (next previous) other-seq)))
+
+(defn levenshtein
+  "Compute the levenshtein distance between two [sequences]."
+  [sequence1 sequence2]
+  (first
+    (reduce (fn [previous current] (next-row previous current sequence2))
+            (map #(identity %2) (cons nil sequence2) (range))
+            sequence1)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Map stuff
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn select-with-merge
+  "Selects subset of a larger configuration map, merging in the selected keys.
+   Selection can be a vector, merge-keys must be a vector. In a conflict, the
+   merge-keys take precedence."
+  [config selection merge-keys]
+  (let [get-or-in (fn [m k]
+                    (if (coll? k)
+                      (get-in m k)
+                      (get m k)))
+        subset    (get-or-in config selection)]
+    (reduce (fn [m k]
+              (assoc m k (get-or-in config k)))
+            subset
+            merge-keys)))
+
+(defn deep-merge
+  "Deeply merges maps so that nested maps are combined rather than replaced.
+  For example:
+  (deep-merge {:foo {:bar :baz}} {:foo {:fuzz :buzz}})
+  ;;=> {:foo {:bar :baz, :fuzz :buzz}}
+  ;; contrast with clojure.core/merge
+  (merge {:foo {:bar :baz}} {:foo {:fuzz :buzz}})
+  ;;=> {:foo {:fuzz :quzz}} ; note how last value for :foo wins"
+  [& vs]
+  (if (every? map? vs)
+    (apply merge-with deep-merge vs)
+    (last vs)))
+
+(defn deep-merge-with
+  "Deeply merges like `deep-merge`, but uses `f` to produce a value from the
+  conflicting values for a key in multiple maps."
+  [f & vs]
+  (if (every? map? vs)
+    (apply merge-with (partial deep-merge-with f) vs)
+    (apply f vs)))
+
+(defn sort-by-ordering
+  "Similar signature as sort-by.
+  Lets the user supply an ordering instead of a comparator fn."
+  [keyfn ordering coll]
+  (let [vals->indexes (into {} (map-indexed (comp vec reverse list) ordering))
+        compfn (fn [v1 v2]
+                 (compare (get vals->indexes v1) (get vals->indexes v2)))
+        ]
+    (sort-by keyfn compfn coll)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Ring HTTP Context map helper fns
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn get-authenticator [ctx]
+  (get-in ctx [:request :authenticator]))
+
+(defn get-auth-fn [ctx]
+  (get-in ctx [:request :authenticator :user-lookup-fn]))
+
+(defn get-data-service [ctx]
+  (get-in ctx [:request :data-service]))
+
+(defn get-web-handler [web-app]
+  (get web-app :app-handler))
+
+(defn get-session-service [ctx]
+  (get-in ctx [:request :session-service]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Ring HTTP Context map helper fns
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn get-literal-or-label [ent k]
+  (or (-> ent (get k) (get :datum/label))
+      (-> ent (get k) (dissoc :db/id :literal/team) (vals) (first))))
+
+(defn fact-entity->tuple [ent]
+  (let [attr (get-literal-or-label ent :fact/attribute)
+        valu (get-literal-or-label ent :fact/value)]
+    (vector attr valu)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; String ID parsing
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn try-read-string [string]
+  (try
+   (#?(:cljs cljs.reader/read-string
+       :clj  read-string)
+            string)
+   (catch #?(:cljs js/Object :clj Exception) e
+     string)))
+
+(defn read-entity-id
+  "Sometimes an integer, sometimes a string, never a symbol.
+  Negative numbers are allowed because they represent temp ids."
+  [string]
+  (if (= string (re-find #"-?[0-9]*" string))
+    (try-read-string string)
+    string))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Transit helpers // should be CLJC, but not needed right now.
+; kanopi.util.local-storage actually re-implements this in CLJS.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#?(:clj
+   (do
+
+    (defn transit-write [data]
+      (let [out (ByteArrayOutputStream. 4096)
+            writer (transit/writer out :json)]
+        (transit/write writer data)
+        (.toString out)))
+
+    (defn transit-read [stream]
+      ;; NOTE: type hint avoids warning when calling .getBytes below
+      (when stream
+        (let [^java.lang.String string (slurp stream)]
+          (if (or (nil? string) (clojure.string/blank? string))
+            {}
+            (let [in (ByteArrayInputStream. (.getBytes string))
+                  reader (transit/reader in :json)]
+              (transit/read reader))))))
+    ))
--- src-cljs/kanopi/controller/handlers/response.cljs
+++ src-cljs/kanopi/controller/handlers/response.cljs
@@ -1,6 +1,5 @@
 (ns kanopi.controller.handlers.response
   (:require [om.core :as om]
-
             [taoensso.timbre :as timbre
              :refer-macros (log trace debug info warn error fatal report)]
 
@@ -10,6 +9,10 @@
             
             [kanopi.util.core :as util]))
 
+(def transact!
+  #(:clj  swap!
+    :cljs om/transact!))
+
 (defmulti local-response-handler
   (fn [_ _ _ msg]
     (info msg)
@@ -17,7 +20,7 @@
 
 (defn- record-error-message
   [app-state msg]
-  (om/transact! app-state :error-messages #(conj % msg)))
+  (transact! app-state :error-messages #(conj % msg)))
 
 (defmethod local-response-handler :spa.navigate/success
   [aether history app-state msg]
@@ -25,27 +28,27 @@
 
 (defn- incorporate-user-datum!
   [app-state user-datum]
-  (om/transact! app-state
-                (fn [app-state]
-                  (let [datum-id (get-in user-datum [:datum :db/id])]
-                    (-> app-state
-                        (assoc :datum user-datum)
-                        (assoc-in [:cache datum-id] (get user-datum :datum)))))))
+  (transact! app-state
+             (fn [app-state]
+               (let [datum-id (get-in user-datum [:datum :db/id])]
+                 (-> app-state
+                     (assoc :datum user-datum)
+                     (assoc-in [:cache datum-id] (get user-datum :datum)))))))
 
 (defn- incorporate-updated-datum!
   [app-state {:keys [datum new-entities]}]
-  (om/transact! app-state
-                (fn [app-state]
-                  (println "incorporate updated datum" (get datum :datum/fact))
-                  (println new-entities)
-                  (let [datum-id (get datum :db/id)
-                        cache-delta (->> (conj new-entities datum)
-                                         (map (comp vec (juxt :db/id identity)))
-                                         (into {}))
-                        app-state' (-> app-state
-                                       (assoc-in [:datum :datum] datum)
-                                       (update :cache #(merge % cache-delta)))]
-                    app-state'))))
+  (transact! app-state
+             (fn [app-state]
+               (println "incorporate updated datum" (get datum :datum/fact))
+               (println new-entities)
+               (let [datum-id (get datum :db/id)
+                     cache-delta (->> (conj new-entities datum)
+                                      (map (comp vec (juxt :db/id identity)))
+                                      (into {}))
+                     app-state' (-> app-state
+                                    (assoc-in [:datum :datum] datum)
+                                    (update :cache #(merge % cache-delta)))]
+                 app-state'))))
 
 (defmethod local-response-handler :datum.fact.add/success
   [aether history app-state msg]
@@ -93,9 +96,9 @@
 (defmethod local-response-handler :spa.navigate.search/success
   [aether history app-state msg]
   (let [{:keys [query-string results]} (get msg :noun)]
-    (om/transact! app-state :search-results
-                  (fn [search-results]
-                    (assoc search-results query-string results)))))
+    (transact! app-state :search-results
+               (fn [search-results]
+                 (assoc search-results query-string results)))))
 
 (defmethod local-response-handler :spa.navigate.search/failure
   [aether history app-state msg]
@@ -104,17 +107,17 @@
 (defmethod local-response-handler :spa.register/success
   [aether history app-state {:keys [noun] :as msg}]
   (let []
-    (om/transact! app-state
-                  (fn [app-state]
-                    (assoc app-state
-                           :user noun
-                           :mode :spa.authenticated/online
-                           :intent {:id :spa.authenticated/navigate}
-                           :datum {:context-datums []
-                                   :datum {}
-                                   :similar-datums []}
-                           :cache {}
-                           :error-messages [])))
+    (transact! app-state
+               (fn [app-state]
+                 (assoc app-state
+                        :user noun
+                        :mode :spa.authenticated/online
+                        :intent {:id :spa.authenticated/navigate}
+                        :datum {:context-datums []
+                                :datum {}
+                                :similar-datums []}
+                        :cache {}
+                        :error-messages [])))
     (history/navigate-to! history :home)
     (->> (msg/initialize-client-state noun)
          (aether/send! aether))))
@@ -126,17 +129,17 @@
 (defmethod local-response-handler :spa.login/success
   [aether history app-state {:keys [noun] :as msg}]
   (let []
-    (om/transact! app-state
-                  (fn [app-state]
-                    (assoc app-state
-                           :user noun
-                           :mode :spa.authenticated/online
-                           :intent {:id :spa.authenticated/navigate}
-                           :datum {:context-datums []
-                                   :datum {}
-                                   :similar-datums []}
-                           :cache {}
-                           :error-messages [])))
+    (transact! app-state
+               (fn [app-state]
+                 (assoc app-state
+                        :user noun
+                        :mode :spa.authenticated/online
+                        :intent {:id :spa.authenticated/navigate}
+                        :datum {:context-datums []
+                                :datum {}
+                                :similar-datums []}
+                        :cache {}
+                        :error-messages [])))
     (history/navigate-to! history :home)
     (->> (msg/initialize-client-state noun)
          (aether/send! aether))))
@@ -148,17 +151,17 @@
 (defmethod local-response-handler :spa.logout/success
   [aether history app-state msg]
   (let []
-    (om/transact! app-state
-                  (fn [app-state]
-                    (assoc app-state
-                           :user nil
-                           :mode :spa.unauthenticated/online
-                           :intent {:id :spa.unauthenticated/navigate}
-                           :datum {:context-datums []
-                                   :datum []
-                                   :similar-datums []}
-                           :error-messages []
-                           :cache {})))
+    (transact! app-state
+               (fn [app-state]
+                 (assoc app-state
+                        :user nil
+                        :mode :spa.unauthenticated/online
+                        :intent {:id :spa.unauthenticated/navigate}
+                        :datum {:context-datums []
+                                :datum []
+                                :similar-datums []}
+                        :error-messages []
+                        :cache {})))
     (history/navigate-to! history :home)))
 
 (defmethod local-response-handler :spa.logout/failure
@@ -168,9 +171,9 @@
 (defmethod local-response-handler :spa.state.initialize/success
   [aether history app-state msg]
   (let []
-    (om/transact! app-state
-                  (fn [app-state]
-                    (merge app-state (get msg :noun))))))
+    (transact! app-state
+               (fn [app-state]
+                 (merge app-state (get msg :noun))))))
 
 (defmethod local-response-handler :spa.state.initialize/failure
   [aether history app-state msg]
@@ -179,8 +182,8 @@
 (defmethod local-response-handler :spa.switch-team/success
   [aether history app-state msg]
   (let [user' (get msg :noun)]
-    (om/transact! app-state (fn [app-state]
-                              (assoc app-state :user (get msg :noun))))
+    (transact! app-state (fn [app-state]
+                           (assoc app-state :user (get msg :noun))))
     ;; NOTE: user changed, therefore creds changed, therefore must
     ;; reinitialize => could have a current-datum which is not
     ;; accessible from the new creds
@@ -190,21 +193,21 @@
 
 (defn incorporate-updated-literal! [app-state literal]
   (println "incorporate-literal:" literal)
-  (om/transact! app-state
-                (fn [app-state]
-                  (let [literal-id (get literal :db/id)]
-                    (-> app-state
-                        (assoc-in [:literal :literal] literal)
-                        (assoc-in [:cache literal-id] literal))))))
+  (transact! app-state
+             (fn [app-state]
+               (let [literal-id (get literal :db/id)]
+                 (-> app-state
+                     (assoc-in [:literal :literal] literal)
+                     (assoc-in [:cache literal-id] literal))))))
 
 (defn incorporate-user-literal! [app-state user-literal]
-  (om/transact! app-state
-                (fn [app-state]
-                  (let [literal-id (get-in user-literal [:literal :db/id])]
-                    (-> app-state
-                        (assoc :literal user-literal)
-                        (assoc-in [:cache literal-id] (get user-literal :literal)))
-                    ))))
+  (transact! app-state
+             (fn [app-state]
+               (let [literal-id (get-in user-literal [:literal :db/id])]
+                 (-> app-state
+                     (assoc :literal user-literal)
+                     (assoc-in [:cache literal-id] (get user-literal :literal)))
+                 ))))
 
 (defmethod local-response-handler :literal.get/success
   [aether history app-state msg]
